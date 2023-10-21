ae5a9cc9fc72b1429e941e90f3511351bb0d7fe7
StevauxLoic <stevaux.loic@gmail.com>
2023-10-21 15:37:07 +0300

    add of ideas (categories and new dict)

---
+++ ideas/dict_idea.py
@@ -0,0 +1,17 @@
+level_1 = [
+        {"question": "1+1 = ?",
+         "answers": {"A":"4", "B":"-1", "C":"2"}, "correct_answer": "C"},
+
+        {"question": "2+3 = ?",
+         "answers": {"A":"5", "B":"10", "C":"8"}, "correct_answer": "A"}
+    ]
+
+level_1 = [
+        {"question": "1+1 = ?",
+         "answers": ["4", "-1"], "correct_answer": "2"},
+    ]
+
+input = 1
+index  = input -1
+
+level_1[0]["answers"][index]
